0dd697df79f3cb59f1dc95915ce1a6c460d8b554
Martin Dupuis <4422549+mdupuis13@users.noreply.github.com>
2022-05-20 16:57:35 -0400

    Upgrade script to mimic polybar-script
    https://github.com/polybar/polybar-scripts/blob/master/polybar-scripts/updates-apt/updates-apt.sh

--- leftwm/.config/leftwm/themes/md-nord/polybar-scripts/updates-apt.sh
+++ leftwm/.config/leftwm/themes/md-nord/polybar-scripts/updates-apt.sh
@@ -2,45 +2,6 @@
 
 #set -xe
 
-message=""
+updates=$(apt list --upgradable 2> /dev/null | grep -c upgradable);
 
-fetchUpdates () {
-  sudo apt-get update 1> /dev/null
-  return $?
-}
-
-getNumberOfNormalUpdates () {
-  updates=$(apt-get upgrade -s |grep -P '^\d+ upgraded'|cut -d" " -f1);
-
-#  if [ "$updates" -gt 0 ]; then
-      message="$updates"
-#  fi
-
-  return 0
-}
-
-#getNumberOfSecurityUpdates () {
-  #updates=0
-  #updates=$(apt-get upgrade -s |grep -P '^\d+ \w\ssecurity'|cut -d" " -f1);
-
-  #if [ ! -z "$updates" ] && [ "$updates" -gt 0 ]; then
-      #message+=" $updates sec"
-  #fi
-
-  #return 0
-#}
-
-printMessage() {
-  echo $message
-  return 0
-}
-
-fetchUpdates
-
-if [ $? -eq 0 ]; then
-  getNumberOfNormalUpdates
-#  getNumberOfSecurityUpdates
-fi
-
-
-printMessage
+echo $updates

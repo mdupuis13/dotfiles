9fae948573690da5fa2d1e68048e39e98a067d37
Martin Dupuis <4422549+mdupuis13@users.noreply.github.com>
2022-11-21 09:10:24 -0500

    Cleanup configs

--- leftwm/.config/leftwm/themes/md-nord/polybar-scripts/system-fan-speed.sh
+++
@@ -1,1 +0,0 @@
-/home/mdupuis/src/github/polybar-scripts/polybar-scripts/system-fan-speed/system-fan-speed.sh
--- leftwm/.config/leftwm/themes/md-nord/polybar-scripts/updates-apt.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-#set -xe
-
-updates=$(apt list --upgradable 2> /dev/null | grep -c upgradable);
-
-echo $updates

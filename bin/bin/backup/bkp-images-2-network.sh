746e53361df8b9fe7f02cd4430432ebaab402849
Martin Dupuis <4422549+mdupuis13@users.noreply.github.com>
2024-02-23 21:26:58 -0500

    Change img bkp script

--- bin/bin/backup/bkp-images-2-network.sh
+++ bin/bin/backup/bkp-images-2-network.sh
@@ -4,7 +4,7 @@ photos_folder=$HOME/Photographie
 echo Synchronisation de $photos_folder vers dasnas
 
 if [[ -d $photos_folder ]]; then
-  rsync --modify-window=2 --prune-empty-dirs --quiet --recursive --update $photos_folder/* /mnt/dasnas/media/Photos/
+  rsync --modify-window=2 --prune-empty-dirs --recursive --update --progress $photos_folder/* /mnt/dasnas/media/Photos/
 
   # --modify-window
   #     When comparing two timestamps, rsync treats the timestamps as being equal if they differ by no more than the modify-window value. This is normally 0 (for an exact match), but you may find it useful to set this to a larger value in some situations. In particular, when transferring to or from an MS Windows FAT filesystem (which represents times with a 2-second resolution), --modify-window=1 is useful (allowing times to differ by up to 1 second). 
@@ -12,9 +12,9 @@ if [[ -d $photos_folder ]]; then
   # -m, --prune-empty-dirs
   #     This option tells the receiving rsync to get rid of empty directories from the file-list, including nested directories that have no non-directory children. This is useful for avoiding the creation of a bunch of useless directories when the sending rsync is recursively scanning a hierarchy of files using include/exclude/filter rules.
 
-  # -q, --quiet
-  #     This option decreases the amount of information you are given during the transfer, notably suppressing information messages from the remote server. This option is useful when invoking rsync from cron. 
-
+  # --progress
+  #   This option tells rsync to print information showing the progress of the transfer. This gives a bored user something to watch. Implies --verbose if it wasn't already specified. 
+    
   # -r, --recursive
   #     This tells rsync to copy directories recursively. See also --dirs (-d). 
 

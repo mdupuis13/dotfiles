740d334a309a8d2160096d6053ad15a825e7762b
Martin Dupuis <4422549+mdupuis13@users.noreply.github.com>
2024-11-09 19:15:38 -0500

    Show percent instead of wattage

--- qtile/.config/qtile/qtilebar-scripts/get-gpu-usage.sh
+++ qtile/.config/qtile/qtilebar-scripts/get-gpu-usage.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-gpuwatts=`sensors amdgpu-pci-0c00 | grep PPT | awk '{ print $2 }' | awk 'BEGIN { FS = "." } ; { print $1}'`
+gpuMaxWatgts=230
+gpuMaxFreq=2474
 
-pctUsage=$(( ($gpuwatts * 100) / 230 )) 
+gpuwatts=`sensors amdgpu-pci-0c00 | grep PPT | awk '{ print $2 }' | awk 'BEGIN { FS = "." } ; { print $1}'`
+gpuPct=`sensors amdgpu-pci-0c00 | grep sclk | awk '{ print $2 }' | awk 'BEGIN { FS = "." } ; { print $1}'`
+
+pctUsage=$(( $gpuPct*100 / gpuMaxFreq )) 
 
 echo -n " <span foreground=\"#61AFEF\">$pctUsage %</span>"

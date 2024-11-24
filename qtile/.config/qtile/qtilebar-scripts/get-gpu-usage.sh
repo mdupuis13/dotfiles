327ec1cbfe9fd2bdd9f8b2297cd1a74074cc7b4c
Martin Dupuis <4422549+mdupuis13@users.noreply.github.com>
2024-11-23 22:36:37 -0500

    Get GPU % util direct from card

--- qtile/.config/qtile/qtilebar-scripts/get-gpu-usage.sh
+++ qtile/.config/qtile/qtilebar-scripts/get-gpu-usage.sh
@@ -3,9 +3,11 @@
 gpuMaxWatgts=230
 gpuMaxFreq=2474
 
-gpuwatts=`sensors amdgpu-pci-0c00 | grep PPT | awk '{ print $2 }' | awk 'BEGIN { FS = "." } ; { print $1}'`
-gpuPct=`sensors amdgpu-pci-0c00 | grep sclk | awk '{ print $2 }' | awk 'BEGIN { FS = "." } ; { print $1}'`
+#gpuWatts=`sensors amdgpu-pci-0c00 | grep PPT | awk '{ print $2 }' | awk 'BEGIN { FS = "." } ; { print $1}'`
+#gpuPwr=`sensors amdgpu-pci-0c00 | grep sclk | awk '{ print $2 }' | awk 'BEGIN { FS = "." } ; { print $1}'`
 
-pctUsage=$(( $gpuPct*100 / gpuMaxFreq )) 
+gpuUsage=`cat /sys/class/drm/card1/device/gpu_busy_percent`
 
-echo -n " <span foreground=\"#61AFEF\">$pctUsage %</span>"
+#pctUsage=$(( $gpuPwr div gpuMaxFreq )) 
+
+echo -n " <span foreground=\"#61AFEF\">$gpuUsage %</span>"
